3a953e682aab10c2ade4121f56d0e3ba691c736e
abdou-rachidou <abdou.rachidou-arouna@laplateforme.io>
2024-01-02 10:55:24 +0100

    Le job02 terminé

---
+++ jour01/job02/main.py
@@ -0,0 +1,13 @@
+class Operation:
+
+
+    def __init__(self, nombre1, nombre2) -> None:
+        self.my_number1 = nombre1
+        self.my_number2 = nombre2
+        pass
+
+    
+        print("Le nombre1 est", nombre1)
+        print("Le nombre2 est", nombre2)
+
+result = Operation(12, 3)
