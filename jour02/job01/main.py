bc46cd3522a09710388c7576af1a566c1a9a617d
abdou-rachidou <abdou.rachidou-arouna@laplateforme.io>
2024-01-03 10:31:03 +0100

    Le job01 du jour02 terminé

---
+++ jour02/job01/main.py
@@ -0,0 +1,35 @@
+class Rectangele:
+
+
+    def __init__(self, longueur, largeur) -> None:
+        self.__longueur = longueur
+        self.__largeur = largeur
+        pass
+
+    # Accesseurs (getters)
+    def get_longueur(self):
+        return self.__longueur
+    
+    def get_largeur(self):
+        return self.__largeur
+    
+    # Mutateurs (setters)
+    def set_longueur(self, longueur_modifiee):
+        self.__longueur = longueur_modifiee
+
+    def set_largeur(self, largeur_modifiee):
+        self.__largeur = largeur_modifiee
+
+rectangle = Rectangele(10, 5)
+
+# Afficher les valeurs initiales
+print("Longueur initiale :", rectangle.get_longueur())
+print("Largeur initiale :", rectangle.get_largeur())
+
+# Modification de la longueur et de la largeur en utilisant les mutateurs
+rectangle.set_longueur(13)
+rectangle.set_largeur(7)
+
+# Afficher les valeurs après modification
+print("Longueur modifiée :", rectangle.get_longueur())
+print("Largeur modifiée :", rectangle.get_largeur())
